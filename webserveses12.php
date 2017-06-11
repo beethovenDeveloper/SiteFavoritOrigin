ca2fef995c1e97ee70f2864a836188c7d8638490
beethoven <beethovendeveloper@gmail.com>
2017-06-11 23:44:13 +0300

    11062017 all day

---
+++ webserveses12.php
@@ -0,0 +1,30 @@
+<?php
+  class InfoClass
+  {
+  	public $infor;
+  }
+  
+  $titN=$_GET["newsTitle"];
+  $fold=opendir("news");
+  $info=new InfoClass();
+  $info->infor="false";
+  while(($fileNext=readdir($fold))!==false)
+  {
+    if(($fileNext==".") || ($fileNext==".."))
+	{
+	}
+	else
+	{
+		$f=file_get_contents("news/".$fileNext);
+		$fileNews=new SimpleXMLElement($f);
+		$str="".$fileNews->xpath("//title")[0];
+		if($str==$titN)
+		{
+             $info->infor="true";
+		}
+	}
+}
+
+
+echo json_encode($info);
+?>
